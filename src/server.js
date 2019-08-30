805af750eb4e9ce58b4f8db288089d563079afcf
lucas-marinoto <lucas_marinoto@msn.com>
2019-08-30 16:41:27 -0300

    alterado server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3333, function() {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
 // app.listen(3333);
