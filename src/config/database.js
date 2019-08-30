2b15c1999799e97e22fc517fd2ff55428d08c3c1
lucas-marinoto <lucas_marinoto@msn.com>
2019-08-30 15:40:32 -0300

    mudança para heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,21 @@
 module.exports = {
+  /*
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
   password: 'admin',
   database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  */
+  dialect: 'postgres',
+  host: 'ec2-54-235-92-244.compute-1.amazonaws.com',
+  username: 'yfxguygfylbzbu',
+  password: '9eb05ea2cc785a6adc5bb82773012b6533c7c0e7fef7aecd02be8f941f6745dd',
+  database: 'd976g7i0rodjh0',
   define: {
     timestamps: true,
     underscored: true,
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
 import app from './app';
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
+// sapp.listen(3333);
