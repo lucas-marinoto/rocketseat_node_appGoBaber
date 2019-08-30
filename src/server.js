a28ef96392dfbc5829c536d946460c28b7b3268e
lucas-marinoto <lucas_marinoto@msn.com>
2019-08-30 16:59:46 -0300

    alterado configuracao servidor

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import app from './app';
 
-const port = process.env.PORT || 3333;
-
-app.listen(port, () => {
-  console.log(`Express server listening on port`, port);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`[The app is running on port ${PORT}`);
 });
 // app.listen(3333);
