e9e2bfe87353d44ebc24b963a0709aee7cf4faf9
lucas-marinoto <lucas_marinoto@msn.com>
2019-09-01 21:03:07 -0300

    Criado rota ScheduleController

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
+import ScheduleControler from './app/controllers/ScheduleControler';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
@@ -29,6 +29,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleControler.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
